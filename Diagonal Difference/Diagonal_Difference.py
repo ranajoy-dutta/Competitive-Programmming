faedeb0f58361214723eb8b478fbad54959ee900
ranajoy-dutta <ranajoydutta7@gmail.com>
2018-02-14 14:20:10 +0530

    Added Readme

--- Diagonal Difference/Diagonal_Difference.py
+++ Diagonal Difference/Diagonal_Difference.py
@@ -1,18 +1,3 @@
-"""Given a square matrix of size , calculate the absolute difference between the sums of its diagonals.
-
-Input Format :: The first line contains a single integer, . The next  lines denote the matrix's rows, with each line containing space-separated integers describing the columns.
-
-Output Format :: Print the absolute difference between the two sums of the matrix's diagonals as a single integer.
-
-Sample Input
-3
-11 2 4
-4 5 6
-10 8 -12
-
-Sample Output
-15
-"""
 def diagonalDifference(a):
     difference = sum(row[i] - row[-i-1] for i, row in enumerate(a))
     return abs(difference)
