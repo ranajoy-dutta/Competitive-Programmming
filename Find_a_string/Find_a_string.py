d91c8464d671a59fd37cc3be58328d49b2f9ae90
Ranajoy Dutta <ranajoydutta7@gmail.com>
2018-07-25 22:32:08 +0530

    From HackerRank
    Practice question of string manipulations in competitive programming
    from www.hackerrank.com

--- Find_a_string/Find_a_string.py
+++ Find_a_string/Find_a_string.py
@@ -1,15 +1,7 @@
-n = int(input())
-big_arr=[]
-while n>0:
-    arr = list(map(str, input().split()))
-    big_arr.append(arr)
-    n-=1
-name = input()
-lst = []
-for i in range(len(big_arr)):
-    if big_arr[i][0]==name:
-        lst=big_arr[i]
-sum=0
-for i in range(1,len(lst)):
-    sum = sum + float(lst[i])
-print("%.2f"%(sum/3))
+def count_substring(string, sub_string):
+    count = 0
+    for i in range(len(string)-len(sub_string)+1):
+        if string[i:i+len(sub_string)] == sub_string:
+            count += 1
+    return count
+
