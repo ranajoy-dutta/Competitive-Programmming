c99562058fa91a37ae3544ab30d43a9566449f21
Ranajoy Dutta <ranajoydutta7@gmail.com>
2018-08-15 15:27:00 +0530

    From Hackerrank
    Solution

--- Text Wrap/Text Wrap.py
+++ Text Wrap/Text Wrap.py
@@ -1,12 +1,2 @@
-from itertools import combinations
-input()
-arr = input().split()
-k = int(input())
-total,count = 0,0
-for i in combinations(arr,k):
-    total += 1
-    for j in i:
-        if j == 'a':
-            count += 1
-            break
-print(count/total)
+def wrap(string, max_width):
+    return textwrap.fill(string, max_width)
