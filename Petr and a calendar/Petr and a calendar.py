0162790cea1716e80a4ec7d6fa6d8189462c7b72
Ranajoy Dutta <ranajoydutta7@gmail.com>
2020-12-10 22:47:45 +0530

    Update Petr and a calendar.py

--- Petr and a calendar/Petr and a calendar.py
+++ Petr and a calendar/Petr and a calendar.py
@@ -2,19 +2,19 @@
     #Use input() to read input from STDIN and use print to write your output to STDOUT
 
 def main():
-    # for _ in range(int(input())):
-    month = int(input())
-    first_day = int(input())
-    # all months except feb will need atleast 5 columns. More columns may be required 
-    # depending on first day of the month.
-    more_columns = first_day - 1
-    if month==2:
-        print(5 if first_day>1 else 4)
-    elif month in [1,3,5,7,8,10,12]:
-        print(6 if first_day>4 else 5)
-    else:
-        print(6 if first_day>5 else 5)
-    return True
+    m = int(input())
+    d = int(input())
+    counter, col = 1, 1
+    days = [0,31,28,31,30,31,30,31,31,30,31,30,31]
+    
+    while (counter<days[m]):
+        counter += 1
+        d += 1
+        if (d>7):
+            d = 1
+            col += 1
+    print(col)
 
 main()
+
 
